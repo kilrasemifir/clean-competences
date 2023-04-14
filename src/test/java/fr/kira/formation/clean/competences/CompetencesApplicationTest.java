407faa1295b3fea78aac153a40ac3e6abbd7fc47
Killian Raoux <killian@semifir.com>
2023-04-14 14:20:03 +0200

    fix: suppression des tests fails

--- src/test/java/fr/kira/formation/clean/competences/CompetencesApplicationTest.java
+++ src/test/java/fr/kira/formation/clean/competences/CompetencesApplicationTest.java
@@ -5,9 +5,4 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CompetencesApplicationTest {
-
-    @Test
-    void main() {
-        throw new RuntimeException("Test not implemented");
-    }
 }
